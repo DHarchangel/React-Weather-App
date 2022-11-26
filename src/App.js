222ab8bd655fe2b85d3a5f3ba19b1b19a80df445
DHarchangel <dhackangel@gmail.com>
2022-11-26 11:25:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <div className="card">
           <h1>Weather Search Engine</h1>
           <Search />
-          
+          <p><a href="https://github.com/DHarchangel/React-Weather-App">Open Source App </a> Build by Angel</p>
         </div>
       </div>
     </div>
