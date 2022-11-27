caccefa8ede0c028034238d7270ca6daa3172065
DHarchangel <dhackangel@gmail.com>
2022-11-27 12:44:08 +0100

    Adjusted the components, making the search engine fully functioning

--- src/Temperature.js
+++ src/Temperature.js
@@ -2,17 +2,31 @@ import React from 'react';
 import './temperature.css';
 
 function Temperature(props) {
+  function formatDate() {
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let day = days[props.data.date.getDay()]
+    let hours = props.data.date.getHours()
+    if (hours < 10) {hours = `0${hours}`}
+    let minutes = props.data.date.getMinutes()
+    if (minutes < 10) {
+      minutes= `0${minutes}`
+    }
+    return (
+      <span> {day} {hours} <span className='units'>:</span>  {minutes} </span>
+    )
+  }
   return (
     <div className='temperature'>
       <div className="city">
-        <h1>{props.city}</h1>
-        <p>Tuesday 18:43, {props.description}</p>
-        <p>Precipitation: <span className='units'>65%</span>, Wind: <span className='units'>{props.wind}km/hr</span></p>
+        <h1>{props.data.city}</h1>
+        <p>{formatDate()}</p>
+        <p>{props.data.description}</p>
+        <p>Wind: <span className='units'>{props.data.wind}km/hr</span></p>
       </div>
       <div className="temp">
-        <div className="img"><img src={props.icon} alt="Weather Icon" /></div>
+        <div className="img"><img src={props.data.icon} alt="Weather Icon" /></div>
         <div>
-          <h1><span className='tempDigit'> {props.temp} </span> <span className='symbol'> <span className='celsius'>℃ </span>|<span className='farenheit'> ℉</span> </span></h1>
+          <h1><span className='tempDigit'> {props.data.temperature} </span> <span className='symbol'> <span className='celsius'>℃ </span>|<span className='farenheit'> ℉</span> </span></h1>
         </div>
 
       </div>
---
+++ src/Weather.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import Forecast from './Forecast';
+import Header from './Header';
+import "./weather.css";
+import Temperature from './Temperature';
+import axios from 'axios';
+
+function Search(props) {
+    function getCity(city) {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+        axios.get(url).then(handleResponse);
+    }
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    function handleResponse(res) {
+
+        console.log(res.data);
+        setWeatherData({
+            ready: true,
+            temperature: Math.round(res.data.main.temp),
+            date: new Date(res.data.dt * 1000),
+            humidity: res.data.main.humidity,
+            icon: `http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`,
+            wind: res.data.wind.speed,
+            description: res.data.weather[0].description,
+            city: res.data.name
+        });
+    }
+
+
+    let [search, setSearch] = useState('Onitsha');
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+       getCity(search)
+
+    }
+    function handleChange(e) {
+        setSearch(e.target.value);
+    }
+    
+    if (weatherData.ready) {
+    return (
+        <div>
+            <Header/>
+            <form className='form' onSubmit={handleSubmit}>
+                <input
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="Enter a City ..."
+                    id='search-box'
+                    autoFocus='on'
+                />
+                <input
+                    type="submit"
+                    placeholder="Enter a City ..."
+                    value="Search"
+                    id='search-btn'
+                />
+            </form>
+            <Temperature data={weatherData}  />
+            <Forecast />
+            <p><a href="https://github.com/DHarchangel/React-Weather-App" target='_blank' rel="noreferrer">Open Source App </a> Build by Angel</p>
+        </div>
+
+        );
+        } else {
+    getCity(search);
+    return 'Loading...';
+  }
+}
+
+export default Search;
